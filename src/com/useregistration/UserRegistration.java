9b15a4a42bc7b997c420f7ce1cd42f0c138488a4
1501299026 <sinumsd98@gmail.com>
2021-05-25 08:55:46 -0700

    ADDED: Email Validation added

--- src/com/useregistration/UserRegistration.java
+++ src/com/useregistration/UserRegistration.java
@@ -15,16 +15,37 @@ public class UserRegistration {
 		 Matcher m = p.matcher(name);
 		 return m.matches();	 	 
 	}
-	public static void main(String[] args) {
+	/**
+	 * email is a function checks the email validity
+	 * there are 3 mandatory  field abc , bridglabz , co
+	 * two optional field   
+	 */
+	public static void email()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Email");
+		String email = sc.nextLine();
+		String regex = "^[abc](.+)[A-Za-z+_.-]+@[bridgelabz](.+)[co](.+)[A-Za-z]$";
+		System.out.println("The email is: " + email);
+	    System.out.println("Check the email is correct? " + email.matches(regex));
+	}
+	/**
+	 * in main method taking the input from the user to validate
+	 * calling the function to perform.
+	 */
+	public static void main(String[] args) 
+	{
 		System.out.println("Welcome in User Registration");
-		Scanner input = new Scanner(System.in); 
+		Scanner sc = new Scanner(System.in); 
 		System.out.print("Enter the your First Name : ");
-		String fname = input.nextLine();	 
+		String fname = sc.nextLine();	 
 	    System.out.println(usernameValidation(fname));
 	    System.out.print("Enter the your Last Name : ");
-		String lname = input.nextLine();	 
+		String lname = sc.nextLine();	 
 	    System.out.println(usernameValidation(lname));
 	    
+		email();
+	 
 	}
 
 }
