1ef609885e2254b59a9faa1c866c8a62cf17056f
1501299026 <sinumsd98@gmail.com>
2021-06-01 18:25:31 -0700

    ADDED: EXxceptionHandlng Added

---
+++ src/main/java/com/userregistration/Exception.java
@@ -0,0 +1,11 @@
+package com.userregistration;
+
+public class Exception {
+
+    public Exception(String message) {
+
+    }
+
+    public void printStackTrace() {
+    }
+}
---
+++ src/main/java/com/userregistration/IligalArgumentException.java
@@ -0,0 +1,11 @@
+package com.userregistration;
+/**
+ * Added a subclass of exception class to print the exception message.
+ */
+public class IligalArgumentException {
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
+
+
